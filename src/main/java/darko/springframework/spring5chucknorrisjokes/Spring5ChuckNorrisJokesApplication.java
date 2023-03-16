99d23f91cbbe8bc01911350a556e0540e8cc2d75
darko <darkotaomi@gmail.com>
2023-03-16 08:53:00 +0100

    Kreirani project Chuck Norris

---
+++ src/main/java/darko/springframework/spring5chucknorrisjokes/Spring5ChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package darko.springframework.spring5chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ChuckNorrisJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5ChuckNorrisJokesApplication.class, args);
+    }
+
+}
