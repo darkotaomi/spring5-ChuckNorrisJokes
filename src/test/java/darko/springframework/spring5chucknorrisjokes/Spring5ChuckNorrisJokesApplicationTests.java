c658a4bd9d3ae9a80422578041ee5d64204d52da
darkotaomi <54977990+darkotaomi@users.noreply.github.com>
2023-03-16 09:20:05 +0100

    Add files via upload
    Kreiran core file

---
+++ src/test/java/darko/springframework/spring5chucknorrisjokes/Spring5ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package darko.springframework.spring5chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ChuckNorrisJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
